42bdf162169f1242860f05244b6f3f99ac38b2c7
Lincoln Bryant <lincolnb@uchicago.edu>
2019-12-20 16:57:05 -0600

    actually start the service

--- start-harvester.sh
+++ start-harvester.sh
@@ -7,3 +7,7 @@ python lib/python*/site-packages/pandaharvester/harvestertest/basicTest.py
 
 # Communication test
 python lib/python*/site-packages/pandaharvester/harvestertest/testCommunication.py
+
+# Start Harvester
+chown -R harvester: /opt/harvester
+python lib/python*/site-packages/pandaharvester/harvesterbody/master.py --pid $PWD/tmp.pid
